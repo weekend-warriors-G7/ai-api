9d2959e63db4af5a1e9ab0e093d464cc87ef376c
GhostNear <ghostnear1841@gmail.com>
2024-11-14 20:35:04 +0200

    Modify code to allow lists

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import os
 from resnet_model.run_model import get_similarity
-from imgur import save_imgur_image
+from imgur import save_imgur_image, get_filename_and_extension
 from flask import Flask, request, jsonify
 from config import CACHE_PATH
 
 app = Flask(__name__)
+
+def compare_image(first_path: str, second_path: str):
+    first_path = save_imgur_image(first_path, CACHE_PATH)       #
+    second_path = save_imgur_image(second_path, CACHE_PATH)     # Get local path for the imgur paths.
+    result, time = get_similarity(first_path, second_path)
+    _, output, _ = get_filename_and_extension(second_path)
+    return output, result
 
 @app.route('/compare', methods=['POST'])
 def compare():
@@ -12,22 +19,28 @@ def compare():
     data = request.get_json()
     if data == None:
         return jsonify('No JSON data specified.')
-    if data["first"] == None or data["second"] == None:
-        return jsonify("No first or second image links specified.")
-    if type(data["first"]) != str or type(data["second"]) != str:
+    if data["source"] == None or data["others"] == None:
+        return jsonify("No source or other image links specified.")
+    if type(data["source"]) != str or type(data["others"]) != list:
         return jsonify("First and second must be strings.")
+    for element in data["others"]:
+        if type(element) != str:
+            return jsonify("A others' value was not a link.")
 
     # Make cache dir if it doesnt exist.
     os.makedirs(CACHE_PATH, exist_ok=True) 
     
     # Let's get the images and cry if we don't.
-    first_path = ""
-    second_path = ""
+    final_result = []
     try:
-        first_path = save_imgur_image(data["first"], CACHE_PATH)
-        second_path = save_imgur_image(data["second"], CACHE_PATH)
-    except Exception as error:
+        for otherImage in data["others"]:
+            identifier, result = compare_image(data["source"], otherImage)
+            element = dict()
+            element["id"] = identifier
+            element["confidence"] = result 
+            final_result.append(element)
+    except error as Exception:
         return jsonify(error.__str__())
-    
-    result, time = get_similarity(first_path, second_path)
-    return jsonify(result)
+
+    final_result.sort(key=lambda x: x["confidence"], reverse=True)
+    return jsonify(final_result)
